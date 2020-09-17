f7ebe171b05aae5f75197171ce246206e46e4dc2
Mikele <smeshmike@yandex,ru>
2020-09-17 21:14:34 +0300

    Add Parallel

--- DFSMaze/MazeForm.cs
+++ DFSMaze/MazeForm.cs
@@ -7,10 +7,11 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DFSMaze.Logic;
-using Encoder = System.Text.Encoder;
+using System.Windows.Media.Imaging;
 
 namespace DFSMaze
 {
@@ -22,7 +23,7 @@ namespace DFSMaze
 
         }
 
-        private ByteGrid byteGrid;
+        private static ByteGrid byteGrid;
         private Point enter;
         private Point exit;
         private Bitmap bitmap;
@@ -31,6 +32,7 @@ namespace DFSMaze
         {
             timeLabel.Text = "";
             var size = Convert.ToInt32(nTextBox.Text);
+            var size2 = (size*2+1) * (size * 2 + 1);
             enter = new Point(Convert.ToInt32(xEnterTextBox.Text), Convert.ToInt32(yEnterTextBox.Text));
             exit = new Point(Convert.ToInt32(xExitTextBox.Text), Convert.ToInt32(yExitTextBox.Text));
             byteGrid = new ByteGrid(Convert.ToUInt32(nTextBox.Text));
@@ -39,12 +41,6 @@ namespace DFSMaze
             stopWatch.Start();
 
             byteGrid.CreateMaze(enter, exit);
-
-
-            //foreach (var point in byteGrid.Way)
-            //{
-            //    pointLabel.Text += $"\n {point.X},{point.Y}";
-            //}
 
             stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;
@@ -56,87 +52,55 @@ namespace DFSMaze
             stopWatch.Start();
 
             zoom = 1;
+
+
+
             bitmap = new Bitmap((2 * size + 1) * zoom, (2 * size + 1) * zoom);
+            Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
 
-            for (int i = 0; i < (2 * size + 1); i +=1)
-            {
-                for (int j = 0; j < (2 * size + 1) ; j +=  1)
-                {
-                    if (!byteGrid.Cell[i, j])
-                    {
-                        DrawCube(zoom, bitmap, i, j, Color.Black);
-                        //bitmap.SetPixel(i, j, Color.Black);
-                    }
-                    else 
-                    {
-                        DrawCube(zoom, bitmap, i, j, Color.AliceBlue);
-                        //bitmap.SetPixel(i, j, Color.AliceBlue);
-                    }
+            BitmapData bmpData = bitmap.LockBits(rect, ImageLockMode.ReadWrite, bitmap.PixelFormat);
 
-                }
-            }
+            var ptr = bmpData.Scan0;
 
-            //if (((i % 2 != 0 && j % 2 != 0) && (i < 2 * size && j < 2 * size)))
+            int bytes = Math.Abs(bmpData.Stride) * bitmap.Height ;
+            int[] tmp = new int[bytes];
+            //Math.Abs(bmpData.Stride)
+            System.Runtime.InteropServices.Marshal.Copy(ptr, tmp, 0, bytes);
+
+            //Parallel.For(0, tmp.Length, delegate(int i) { tmp[i] = 500000; });
+            DrawIntoBitmap(tmp, 0, size, 0, size);
+
+
+            System.Runtime.InteropServices.Marshal.Copy(tmp, 0, ptr, bytes);
+            bitmap.UnlockBits(bmpData);
 
             foreach (var point in byteGrid.WayOut1)
             {
-                DrawCube(zoom, bitmap, point.X, point.Y, Color.CornflowerBlue);
-                //bitmap.SetPixel(point.X , point.Y, Color.CornflowerBlue);
+                //DrawCube(zoom, bitmap, point.X, point.Y, Color.CornflowerBlue);
+                bitmap.SetPixel(point.X , point.Y, Color.CornflowerBlue);
             }
 
-            DrawCube(zoom, bitmap, enter.X * 2 + 1, enter.Y * 2 + 1, Color.Red);
-            DrawCube(zoom, bitmap, exit.X * 2 + 1, exit.Y * 2 + 1, Color.Red);
-            
+            bitmap.SetPixel(enter.X * 2 + 1, enter.Y * 2 + 1, Color.Red);
+            bitmap.SetPixel(exit.X * 2 + 1, exit.Y * 2 + 1, Color.Red);
 
-            ImageCodecInfo myImageCodecInfo = GetEncoderInfo("image/jpeg");
+            //Draw(size);
 
-            var myEncoder = System.Drawing.Imaging.Encoder.Quality;
-            // Save the bitmap as a JPEG file with quality level 25.
-            var myEncoderParameter = new EncoderParameter(myEncoder, 25L);
-            EncoderParameters myEncoderParameters = new EncoderParameters();
-            myEncoderParameters.Param[0] = myEncoderParameter;
 
-            myEncoderParameter = new EncoderParameter(myEncoder, 100L);
-            myEncoderParameters.Param[0] = myEncoderParameter;
-            bitmap.Save("Shapes100.jpg", myImageCodecInfo, myEncoderParameters);
+            SavePicture();
+
 
             stopWatch.Stop();
             ts = stopWatch.Elapsed;
             text += $"\n\n\nВремя\nотрисовки:\n{ts.Minutes:00}:{ts.Seconds:00}.{ts.Milliseconds:000000}";
             timeLabel.Text += text;
 
-            //byteGrid.FindWayOut(enter, exit);
-
-            //foreach (var point in byteGrid.Way)
-            //{
-            //    if((point.X==enter.X * 2 + 1 && point.Y == enter.Y * 2 + 1) || (point.X == exit.X * 2 + 1 && point.Y == exit.Y * 2 + 1))
-            //    {
-            //        DrawCube(zoom, bitmap, point.X, point.Y, Color.Green);
-
-            //    }
-            //    else
-            //    {
-            //        DrawCube(zoom, bitmap, point.X, point.Y, Color.Red);
-            //    }
-            //    //pointLabel.Text += $"\n {point.X},{point.Y}";
-
-            //}
-
-            //pictureBox.Image = bitmap;
-            //pictureBox.Show();
-        }
-
-
-        public void DrawCube(int zoom, Bitmap bitmap, int x, int y, Color color)
-        {
-            for (int i = 0; i < zoom; i++)
+            async void Draw(int size1)
             {
-                for (int j = 0; j < zoom; j++)
-                {
-                    bitmap.SetPixel(x*zoom+i,y*zoom+j, color);
-                }   
+                //var t1 = Task.Run(() => DrawIntoBitmap(tmp, 0, size1, 0, size1));
+                //await Task.WhenAll(t1, t2);
             }
         }
+
 
         private static ImageCodecInfo GetEncoderInfo(String mimeType)
         {
@@ -149,6 +113,45 @@ namespace DFSMaze
                     return encoders[j];
             }
             return null;
+        }
+
+        private static void DrawIntoBitmap(int[] bitmap, int iStart, int iStop, int jStart, int jStop)
+        {
+            Parallel.For(2 * iStart + 1, 2 * iStop + 1, (i) =>
+            {
+                Parallel.For(2 * jStart + 1, 2 * jStop + 1, (j) =>
+                {
+                    if (!byteGrid.Cell[i, j])
+                    {
+                        //DrawCube(zoom, bitmap, i, j, Color.Black);
+                        //bitmap.SetPixel(i, j, Color.Red);
+                        bitmap[j * (2 * iStop + 1) + i] = Color.Black.ToArgb();
+                    }
+                    else
+                    {
+                        //DrawCube(zoom, bitmap, i, j, Color.AliceBlue);
+                        //bitmap.SetPixel(i, j, Color.AliceBlue);
+                        bitmap[j * (2 * iStop + 1) + i] = Color.AliceBlue.ToArgb();
+                    }
+                });
+
+            });
+        }
+
+
+
+        private void SavePicture()
+        {
+            ImageCodecInfo myImageCodecInfo = GetEncoderInfo("image/jpeg");
+
+            var myEncoder = System.Drawing.Imaging.Encoder.Quality;
+            var myEncoderParameter = new EncoderParameter(myEncoder, 25L);
+            EncoderParameters myEncoderParameters = new EncoderParameters();
+            myEncoderParameters.Param[0] = myEncoderParameter;
+
+            myEncoderParameter = new EncoderParameter(myEncoder, 100L);
+            myEncoderParameters.Param[0] = myEncoderParameter;
+            bitmap.Save("Shapes100.jpg", myImageCodecInfo, myEncoderParameters);
         }
     }
 }
